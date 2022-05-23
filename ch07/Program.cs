0b3c65ef8c0f1d1ba34c4284eb54d56f7683bd9a
jandershock <jordan.andershock@gmail.com>
2022-05-23 09:56:36 -0500

    Finished chapter 7

---
+++ ch07/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ch07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("The quick brown");
+            Console.WriteLine("fox jumps");
+            Console.Write("over the");
+            Console.WriteLine("lazy dog");
+
+            // either
+            string input = Console.ReadLine();
+
+            int parsedInput = int.Parse(input);
+            Console.WriteLine($"Int is: {parsedInput}");
+
+            //or
+            string input2 = Console.ReadLine();
+
+            double parsedInput2 = Double.Parse(input2);
+            Console.WriteLine($"Double is: {parsedInput2}");
+        }
+    }
+}
